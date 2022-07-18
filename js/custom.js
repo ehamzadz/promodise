86bcdaeffa37c6739983aa028744881fb5e5644d
ehamzadz <man.hamzadz@gmail.com>
2022-07-18 18:28:57 +0100

    Localisation Update

--- js/custom.js
+++ js/custom.js
@@ -76,7 +76,7 @@
             var mapOptions = {
               zoom: 15,
               scrollwheel: false,
-              center: new google.maps.LatLng(40.7127837, -74.00594130000002)
+              center: new google.maps.LatLng(36.0685452, 4.7502804)
             };
             var map = new google.maps.Map(document.getElementById('map'),
                 mapOptions);
